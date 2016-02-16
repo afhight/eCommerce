74eb06b55372e7f09a343ab3781780dbc4877957
Alison Hight <afhight@gmail.com>
2016-02-16 11:19:23 -0500

    added cart controller and line items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :categories
+  before_filter :brands
+
+  def categories
+  	@categories = Category.order(:name)
+  end 
+
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq!
+  		if @brands == nil
+  			@brands = Product.pluck(:brand).sort
+  		end
+  end
+
 end
---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,22 @@
+class CartController < ApplicationController
+  before_filter :authenticate_user!, except: [:add_to_cart, :view_order]
+
+  def add_to_cart
+  	line_item = LineItem.new
+  	line_item.product_id = params[:product_id]
+  	line_item.quantity = params[:quantity]
+  	line_item.save
+
+  	line_item.line_item_total = line_item.product.price * line_item.quantity
+  	line_item.save
+
+  	redirect_to root_path
+  end
+
+  def view_order
+  	@line_items = LineItem.all
+  end
+
+  def checkout
+  end
+end
